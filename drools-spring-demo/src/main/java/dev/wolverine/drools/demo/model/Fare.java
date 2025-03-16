09e026998d1364be4c36ed1514df3fdaa7db80dc
vvolverine <pheodor.paramonov@gmail.com>
2025-03-16 19:50:04 +0000

    Added first rule

---
+++ drools-spring-demo/src/main/java/dev/wolverine/drools/demo/model/Fare.java
@@ -0,0 +1,22 @@
+package dev.wolverine.drools.demo.model;
+
+public class Fare {
+    private Long nightSurcharge;
+    private Long rideFare;
+
+    public Long getNightSurcharge() {
+        return this.nightSurcharge;
+    }
+
+    public void setNightSurcharge(Long nightSurcharge) {
+        this.nightSurcharge = nightSurcharge;
+    }
+
+    public Long getRideFare() {
+        return this.rideFare;
+    }
+
+    public void setRideFare(Long rideFare) {
+        this.rideFare = rideFare;
+    }
+}
